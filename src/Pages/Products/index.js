16c4566cab52275bc2419bb84feed0f287a3e5f9
LarryTheGeek <karanilarry@gmail.com>
2019-11-27 18:52:19 +0300

    Add pagination

--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,60 +1,83 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
-import ProductCard from '../../Components/ProductCard'
-import './style.css'
+import ProductCard from "../../Components/ProductCard";
+import "./style.css";
 export default class Products extends Component {
-    state = {
-        preview_products: null,
-        isLoading: false
-    }
-    componentDidMount () {
-        this.getProduct()
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: null,
+      visible: 12,
+      isLoading: false,
+      loadmore: false
+    };
 
-    /**
-     * gets a list of products 
-     */
+    this.loadMore = this.loadMore.bind(this);
+  }
+  async componentDidMount () {
+    await this.getProduct();
+  }
 
-    getProduct = async () => {
-        await this.setState({
-            isLoading: true
-        })
+  /**
+   * gets a list of products
+   */
 
-        //url to make the post requets to
-        const URL = "https://skygarden.search.windows.net/indexes/dev-productsv3/docs/search?api-version=2017-11-11"
+  getProduct = async () => {
+    await this.setState({
+      isLoading: true
+    });
 
-        const  data = {     
-        "filter": "offer_benefit_type eq 'Percentage'",
-        "select": "title, partner_name, thumbnail, stock_record_price_currency, stock_record_price_retail, offer_benefit_type, offer_benefit_value"
-       }
+    //url to make the post requets to
+    const URL =
+      "https://skygarden.search.windows.net/indexes/dev-productsv3/docs/search?api-version=2017-11-11";
 
-        const headers = {
-            "api-key": "4F2408C83BBB69BB31AE97737ED6EE2F"
-        }
-        await axios
-          .post(URL, data, { headers: headers })
-          .then(response => {
-           this.setState({
-             isLoading: false,
-             products: response.data.value,
-             preview_products: response.data.value.slice(0, 12)
-           });
-           console.log("Success", response.data.value.slice(0, 10));
-          })
-          .catch(error => {
-           console.log('Error', error)
-          });
-    }
-    render() {
-        const product_list = this.state.products
-        const {preview_products} = this.state
-        return (
-          <div className="products">
-            {preview_products &&
-              preview_products.map(product => (
-                <ProductCard product={product} />
-              ))}
-          </div>
-        );
-    }
+    const data = {
+      filter: "offer_benefit_type eq 'Percentage'",
+      select:
+        "title, partner_name, thumbnail, stock_record_price_currency, stock_record_price_retail, offer_benefit_type, offer_benefit_value"
+    };
+
+    const headers = {
+      "api-key": "4F2408C83BBB69BB31AE97737ED6EE2F"
+    };
+    await axios
+      .post(URL, data, { headers: headers })
+      .then(response => {
+        this.setState({
+          isLoading: false,
+          products: response.data.value
+        });
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  };
+
+  loadMore = () => {
+    this.setState(prev => {
+      return { visible: prev.visible + 12 };
+    });
+  };
+  render() {
+    const count = this.state.visible;
+    const { products } = this.state;
+    let product_list = products ? products.slice(0, count) : 0;
+     console.log(product_list);
+    return (
+      <div className="wrapper">
+        <div className="products">
+          { product_list  && product_list.map((product, index) => (
+              <ProductCard product={product} key={index} />
+            ))}
+        </div>
+        <button
+          type="button"
+          className="btn btn-success btn-lg"
+          onClick={this.loadMore}
+        >
+          LOAD MORE
+        </button>
+      </div>
+    );
+  }
 }
