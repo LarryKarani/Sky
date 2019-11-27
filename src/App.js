bb9a294db6a9b375120c9a5e62c96ac69c208e87
LarryTheGeek <karanilarry@gmail.com>
2019-11-27 11:22:47 +0300

    Fetch all the available products
    Create product card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-import TobBar from './Components/BlackTopBar'
-import Banner from './Components/AddsBanner'
+import React from "react";
+import "./App.css";
+import TobBar from "./Components/BlackTopBar";
+import Banner from "./Components/AddsBanner";
+import Products from "./Pages/Products";
 function App() {
   return (
     <div className="app">
-      <TobBar/>
-      <Banner/>
+      <TobBar />
+      <Banner />
+      <Products />
     </div>
   );
 }
